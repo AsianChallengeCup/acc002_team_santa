27f77b98cedec055ee0a1224e416ecc43170815c
Khuvit <161049966+Khuvit@users.noreply.github.com>
2024-12-24 18:12:27 +0900

    testt

---
+++ react-sample-app2/frontend/src/router.js
@@ -0,0 +1,7 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import MyAccount from "./pages/MyAccount/MyAccount";
+import MyBingo from "./pages/MyBingo/MyBingo";
+import Review from "./pages/Review/Review";
+import CreateBingoTable from "./pages/CreateBingoTable/CreateBingoTable";
+import SignUpForm from "./pages/SignUpForm/SignUpForm";

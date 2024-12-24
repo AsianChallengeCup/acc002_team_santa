2d30228ba41435cbf62a42cb8a68939e68a87961
ab-nagai-yuma <106311835+ab-nagai-yuma@users.noreply.github.com>
2024-12-24 18:39:53 +0900

    gitリセット

--- react-sample-app2/frontend/src/router.js
+++
@@ -1,7 +0,0 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import MyAccount from "./pages/MyAccount/MyAccount";
-import MyBingo from "./pages/MyBingo/MyBingo";
-import Review from "./pages/Review/Review";
-import CreateBingoTable from "./pages/CreateBingoTable/CreateBingoTable";
-import SignUpForm from "./pages/SignUpForm/SignUpForm";
